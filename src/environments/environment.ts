ccb18649548f25aa6f8b721ae2d33a685263d4fe
mi-ano <ryanmiano68@gmail.com>
2019-07-15 12:47:25 +0300

    added texts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   apiUrl: "https://api.github.com/",
   accessToken:'37a4a01e2dbd186f34f4776fa68e3b390edd5bca'
 };
