ba311497d2edc4632256a24c730276d205d37d07
mi-ano <ryanmiano68@gmail.com>
2019-07-15 10:28:45 +0300

    added texts

--- src/app/search-params.spec.ts
+++ src/app/search-params.spec.ts
@@ -2,6 +2,6 @@ import { SearchParams } from './search-params';
 
 describe('SearchParams', () => {
   it('should create an instance', () => {
-    expect( new SearchParams).toBeTruthy();
+    expect( SearchParams().toBeTruthy();
   });
 });
